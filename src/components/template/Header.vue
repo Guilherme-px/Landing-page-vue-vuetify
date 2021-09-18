a6e617f8a0ea15d539be6eaf573b7e8c28e15063
Guilherme Augusto <guilherme.s.goncalves@outlook.com>
2021-09-18 04:10:07 -0300

    add README

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,6 +1,20 @@
 <template>
     <section>
-        <v-navigation-drawer v-model="drawer" temporary dark app class="pt-10 grey darken-4">
+        <v-navigation-drawer v-model="drawer" temporary dark app class="grey darken-4">
+            <v-list>
+                <v-list-item class="d-flex flex-column pt-9">
+                    <v-list-item-avatar>
+                        <img
+                            src="https://cdn.pixabay.com/photo/2014/04/02/16/23/coffee-307147_960_720.png"
+                            alt="logo"
+                        />
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title class="title">Cyber coffee</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-divider></v-divider>
             <v-list v-for="(item, i) in items" :key="i" class="pa-0"> 
                 <router-link :to="{hash: item.hash}">        
                     <v-list-item link v-show="isXs" >
@@ -18,6 +32,14 @@
             </v-list>
         </v-navigation-drawer>
         <v-app-bar :reSize="reSize" dark app :color="color" :class="{ sizeY: reSize }">
+            <v-toolbar-title class="pl-6" v-show="!isXs">
+                    <v-img
+                        src="https://cdn.pixabay.com/photo/2014/04/02/16/23/coffee-307147_960_720.png"
+                        max-height="50"
+                        max-width="50"
+                    />
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="isXs"></v-app-bar-nav-icon>
             <v-container class="d-flex flex-row justify-end mr-0" v-else>
                 <router-link v-for="(item, i) in items" :key="i" :to="{hash: item.hash}">
@@ -84,8 +106,6 @@ export default {
 </script>
 
 <style scoped>
-.v-tool
-
 .sizeY {
   height: 80px !important;
   padding-top: 10px;
